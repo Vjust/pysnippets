6beab781edd4b70e422aa57abd15cdb4ebd1cd46
Vijay <vjust@vattitude.(none)>
2012-03-25 09:43:48 -0400

    new files

---
+++ test_email.py
@@ -0,0 +1,39 @@
+import sys
+import re
+
+pat=sys.argv[1]
+input_line = sys.argv[2]
+print '\n Current pattern :%s\n' % (pat)
+print '\n Current input-line:%s\n' % (input_line)
+
+
+
+matches=re.findall(pat, input_line)
+print "\n these are the matches:"
+for m in matches:
+    print "\t\tm = %s" % ( str(m) )
+    print "\n phone - %s" % str(m)
+
+
+#a=re.compile(r"""[(]{0,1} # an optional paren
+#                 (\d{3})  # first group - 3 digits
+#                 [)]{0,1} # closing parens
+#                 \s* # any number of spaces
+#                 [-]* # any hypens
+#                 \s* # any number of spaces
+#                 (\d{3,3}) # second group of digits
+#                 \s*-\s* # any white space 
+#                 (\d{4,4}) # group of 4 digits
+#                 """ , re.X)
+#
+#matches2=a.findall(input_line)
+#print "\n (2) these are the matches:"
+#for m in matches2:
+#    print "\t\tm = %s" % ( str(m) )
+#    phone="%s-%s-%s" % m
+#    print "\n Phone = " + phone
+#
+#for i in m:
+#    print i
+#
+#print type(m)
